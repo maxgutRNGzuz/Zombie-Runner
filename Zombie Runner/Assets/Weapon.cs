0d367ce88f0a6103444d159a1818d794687d4a0f
maxgutRNGzuz <florian01.yan@gmail.com>
2021-08-19 17:29:17 +0200

    159. Introduction to Raycasting

---
+++ Zombie Runner/Assets/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+        print("Player hit: " + hit.transform.name);
+    }
+}
