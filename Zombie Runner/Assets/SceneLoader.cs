a261565db5430f75854b58d5c9275c8ee9d7186a
maxgutRNGzuz <florian01.yan@gmail.com>
2021-08-20 19:45:37 +0200

    170. Game Over UI

---
+++ Zombie Runner/Assets/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI respawnText;
+    [SerializeField] float respawnCountdown = 10f;
+
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
