8897c68b4e69f6cc551896129fa8a30df64e0b8f
maxgutRNGzuz <florian01.yan@gmail.com>
2021-08-23 21:04:12 +0200

    Cool Scope Effect with Brackeys scope vid

--- Zombie Runner/Assets/Player/Weapons/WeaponZoom.cs
+++ Zombie Runner/Assets/Player/Weapons/WeaponZoom.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class WeaponZoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Animator animator = null;
+    [SerializeField] float transitionDuration = 0.1f;
+    [SerializeField] GameObject reticle = null; //crosshair when not scoped
+    [SerializeField] GameObject scopeOverlay = null;
+    [SerializeField] GameObject weaponCamera = null;
+    [SerializeField] Camera mainCamera = null;
+    [SerializeField] float scopedFOV = 40f;
 
-    // Update is called once per frame
+    bool isScoped = false;
+    float normalFOV;
+
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire2"))
+        {
+            isScoped = !isScoped;
+            animator.SetBool("isScoped", isScoped);
+
+            
+            if (isScoped == true)
+            {
+                StartCoroutine(OnScoped());
+            }
+            else
+            {
+                OnUnscoped();
+            }
+        }
+    }
+
+    IEnumerator OnScoped()
+    {
+        reticle.SetActive(false);
+
+        yield return new WaitForSeconds(transitionDuration);
+
+        scopeOverlay.SetActive(true);
+        weaponCamera.SetActive(false);
+
+        normalFOV = mainCamera.fieldOfView;
+        mainCamera.fieldOfView = scopedFOV;
+    }
+
+    void OnUnscoped()
+    {
+        reticle.SetActive(true);
+        scopeOverlay.SetActive(false);
+        weaponCamera.SetActive(true);
+
+        mainCamera.fieldOfView = normalFOV;
     }
 }
