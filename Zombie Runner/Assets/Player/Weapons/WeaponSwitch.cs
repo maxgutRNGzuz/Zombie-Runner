a142d0e858f0ef44505af57c308f4aaa286cc493
maxgutRNGzuz <florian01.yan@gmail.com>
2021-08-27 14:08:42 +0200

    180. Set active weapon

---
+++ Zombie Runner/Assets/Player/Weapons/WeaponSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour
+{
+    [SerializeField] GameObject[] weapons = null;
+    
+    int currentWeaponIndex = 0;
+
+    void Start()
+    {
+        SetWeaponActive();
+    }
+
+    void SetWeaponActive()
+    {
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            if(i == currentWeaponIndex)
+            {
+                weapons[i].SetActive(true);
+            }
+            else
+            {
+                weapons[i].SetActive(false);
+            }
+        }
+    }
+
+}
